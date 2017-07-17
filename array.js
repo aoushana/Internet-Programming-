73fae196609b869c222bc6c586ddbfce0639ed8a
Adrena Oushana <adrenaoushana@Adrenas-MacBook-Air.local>
2017-07-16 21:33:12 -0700

    adding all chapters

---
+++ array.js
@@ -0,0 +1,25 @@
+//arrays
+
+var nums = [2, 5, 8, 12];
+var animals = ["cat", "fox", "dog", "monkey"];
+
+animals.indexOf("dog");
+animals.indexOf("monkey");
+animals.indexOf("cat");
+
+
+var arr = [2, 3, 5];
+arr.push(0);
+
+
+
+//random
+// function myAlert(message) {
+//   if (message === "hi") {
+//     alert("hello");
+//   } else {
+//
+//     alert(message);
+//   }
+// };
+// myAlert("hi");
